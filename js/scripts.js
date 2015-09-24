bc6844170781598b24463231df096f4d7e73234d
YHop <ypeng.tw@gmail.com>
2015-09-24 00:53:54 -0700

    Add all moive list and append to UI

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,15 @@ var youthPrice;
 var adultPrice;
 var seniorPrice;
 
-var movie = function(filmName, rating){
+function Movie (filmName, rating, imgUrl, showTime, timeValue){
   this.filmName = filmName;
   this.rating = rating;
-};
+  this.imgUrl = imgUrl;
+  this.showTime = showTime;
+  this.timeValue = timeValue;
+}
 
-movie.prototype.ratingCheck = function() {
+Movie.prototype.ratingCheck = function() {
   if(this.rating === "R") {
     return true;
   } else {
@@ -15,7 +18,18 @@ movie.prototype.ratingCheck = function() {
   }
 };
 
-var setMatineePrices = function(){
+var appendMovie = function(movie) {
+  debugger;
+  var text = "<li class='col-md-4 thumbnail'><div><center><img src='" + movie.imgUrl + "' alt='" + movie.filmName + "'></center><div class='caption'><center><h3>" + movie.filmName + "</h3></center><button class='btn btn-primary btn-block btn-modal' data-toggle='modal' data-target='#videoModal'>Watch the trailer!</button><center>";
+  for (var i in movie.showTime){
+    text += "<button class='showTime btn btn-primary' value='" + movie.timeValue[i] + "' data-toggle='modal' data-target='#frontModal'>" + movie.showTime[i] + "</button>";
+  }
+
+  text += "</center></div></div></div></li>";
+  $("ul#moiveList").append(text);
+};
+
+var setMatineePrices = function() {
     youthPrice = 5;
     adultPrice = 5;
     seniorPrice = 5;
@@ -53,10 +67,25 @@ var seniorResult = 0;
 var totalPurchase = 0;
 $("#purchased").hide();
 
+var blackmass = new Movie ("Black Mass", "R", "img/blackmass.jpg", ["4:30p", "6:20p", "8:10p", "10:00p"], ["1630", "1820", "2010", "2200"]);
+var everest = new Movie ("Everest iMax 3D", "PG-13", "img/everest_imax.jpg", ["1:00p", "4:05p", "7:05p", "10:05p"], ["1300", "1605", "1905", "2205"]);
+var grandma = new Movie ("Grandma", "NR", "img/grandmaposter.jpg", ["2:00p", "4:10p", "6:50p", "9:00p"], ["1400", "1610", "1850", "2100"]);
+var minions2 = new Movie ("Minions 2", "PG", "img/minionsposter2.jpg", ["2:30p", "4:20p", "6:10p", "9:00p"], ["1430", "1620", "1810", "2100"]);
+var scorchtrials = new Movie ("Scorch Trials", "PG-13", "img/scorchtrialsposter.jpg", ["3:30p", "5:20p", "7:10p", "9:40p"], ["1530", "1720", "1910", "2140"]);
+
+var movieList = [];
+movieList.push(blackmass);
+movieList.push(everest);
+movieList.push(grandma);
+movieList.push(minions2);
+movieList.push(scorchtrials);
+
+for (var i in movieList){
+  appendMovie(movieList[i]);
+}
+
 $(".showTime").click(function(){
-  console.log($(this));
   var time = parseInt($(this).val());
-  console.log(time);
   if(isMatinee(time)){
     setMatineePrices();
   } else {
@@ -104,8 +133,18 @@ $( ".senior_ticket" ).keyup(function() {
     $(".totalPurchase").text(totalPurchase);
   });
 
-  $("#clear").click(function() {
+  $("#stopVideo").click(function() {
     stopVideo();
+  });
+
+  $("#clear").click(function() {
+    debugger;
+    $(".youth_price").empty();
+    $(".adult_price").empty();
+    $(".senior_price").empty();
+    $(".totalPurchase").empty();
+    $("#buyTicket").show();
+    $("#purchased").hide();
     $("input.youth_ticket").val("");
     $("input.adult_ticket").val("");
     $("input.senior_ticket").val("");
